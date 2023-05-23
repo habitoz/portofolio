5ff9999f5013e84f07148d4e1843b759e1c70b49
haben mehar <habenmehari71@gmail.com>
2023-05-23 11:02:15 +0300

    portofolio mobile menu added including respective js file

---
+++ script.js
@@ -0,0 +1,14 @@
+const humberger= document.getElementById('humberger');
+const menu = document.getElementById('menu');
+const menuClose= document.getElementById('menu-close');
+
+const showMenu=()=>{
+    console.log('show menu');
+  menu.classList.add('show-menu')
+}
+const hideMenu=()=>{
+  menu.classList.remove('show-menu')
+}
+
+humberger.addEventListener('click',showMenu)
+menuClose.addEventListener('click',hideMenu)
