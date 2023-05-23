a386ef338e7bea0e0630cad1e6fc374fa21acffd
haben mehar <habenmehari71@gmail.com>
2023-05-23 11:28:35 +0300

    linter eslintrc setup and fix hint errors

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const humberger= document.getElementById('humberger');
+const humberger = document.getElementById('humberger');
 const menu = document.getElementById('menu');
-const menuClose= document.getElementById('menu-close');
+const menuClose = document.getElementById('menu-close');
 
-const showMenu=()=>{
-  menu.classList.add('show-menu')
-}
-const hideMenu=()=>{
-  menu.classList.remove('show-menu')
-}
+const showMenu = () => {
+  menu.classList.add('show-menu');
+};
+const hideMenu = () => {
+  menu.classList.remove('show-menu');
+};
 
-humberger.addEventListener('click',showMenu)
-menuClose.addEventListener('click',hideMenu)
+humberger.addEventListener('click', showMenu);
+menuClose.addEventListener('click', hideMenu);
