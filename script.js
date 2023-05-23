b2dde08e9b664815b0527c3567e6fc56c4ae6989
haben mehar <habenmehari71@gmail.com>
2023-05-23 13:14:48 +0300

    menu items navigation updated

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const humberger = document.getElementById('humberger');
 const menu = document.getElementById('menu');
 const menuClose = document.getElementById('menu-close');
+const menuItemsNode = document.getElementById('menu-items').childNodes;
 
 const showMenu = () => {
   menu.classList.add('show-menu');
@@ -11,3 +12,4 @@ const hideMenu = () => {
 
 humberger.addEventListener('click', showMenu);
 menuClose.addEventListener('click', hideMenu);
+menuItemsNode.forEach((menuItem) => menuItem.addEventListener('click', hideMenu));
